a6fa12a93bf23a09a387744db670dfb91f586ec9
229898 Janiszewski Łukasz <229898@edu.p.lodz.pl>
2021-02-09 16:58:49 +0100

    Completed Stage 3 of the project. - game now enables user to play the game as long as he wants

--- Rock-Paper-Scissors/task/rps/game.py
+++ Rock-Paper-Scissors/task/rps/game.py
@@ -1,26 +1,37 @@
 import random
 
-choose = input()
-option = ["scissors", "paper", "rock"]
-computerChoice = random.choice(option)
-if (choose == "scissors"):
-    if computerChoice == "scissors":
-        print(f'There is a draw ({computerChoice})')
-    elif computerChoice == "paper":
-        print(f'Well done. The computer chose {computerChoice} and failed')
+userInput = input()
+options = ["scissors", "paper", "rock"]
+while userInput != "!exit":
+    while userInput not in options:
+        if userInput == "!exit":
+            break
+        print("Invalid input")
+        userInput = input()
+    if userInput == "!exit":
+        break
+    computerChoice = random.choice(options)
+    if userInput == "scissors":
+        if computerChoice == "scissors":
+            print(f'There is a draw ({computerChoice})')
+        elif computerChoice == "paper":
+            print(f'Well done. The computer chose {computerChoice} and failed')
+        else:
+            print(f'Sorry, but the computer chose {computerChoice}')
+    elif userInput == "paper":
+        if computerChoice == "scissors":
+            print(f'Sorry, but the computer chose {computerChoice}')
+        elif computerChoice == "paper":
+            print(f'There is a draw ({computerChoice})')
+        else:
+            print(f'Well done. The computer chose {computerChoice} and failed')
     else:
-        print(f'Sorry, but the computer chose {computerChoice}')
-elif (choose == "paper"):
-    if computerChoice == "scissors":
-        print(f'Sorry, but the computer chose {computerChoice}')
-    elif computerChoice == "paper":
-        print(f'There is a draw ({computerChoice})')
-    else:
-        print(f'Well done. The computer chose {computerChoice} and failed')
-else:
-    if computerChoice == "scissors":
-        print(f'Well done. The computer chose {computerChoice} and failed')
-    elif computerChoice == "paper":
-        print(f'Sorry, but the computer chose {computerChoice}')
-    else:
-        print(f'There is a draw ({computerChoice})')
+        if computerChoice == "scissors":
+            print(f'Well done. The computer chose {computerChoice} and failed')
+        elif computerChoice == "paper":
+            print(f'Sorry, but the computer chose {computerChoice}')
+        else:
+            print(f'There is a draw ({computerChoice})')
+    userInput = input()
+
+print("Bye!")
