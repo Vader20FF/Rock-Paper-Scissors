dc9bb89ad865bb1c6cfe83f4ec0741d36079ce26
229898 Janiszewski Łukasz <229898@edu.p.lodz.pl>
2021-02-09 14:18:59 +0100

    Initial commit

---
+++ Rock-Paper-Scissors/task/rps/game.py
@@ -0,0 +1,7 @@
+choose = input()
+if (choose == "scissors"):
+    print("Sorry, but the computer chose rock")
+elif (choose == "paper"):
+    print("Sorry, but the computer chose scissors")
+else:
+    print("Sorry, but the computer chose paper")
