c4a57e46b0238346e8f97f3f489c78d57c42bb2f
229898 Janiszewski Łukasz <229898@edu.p.lodz.pl>
2021-02-09 16:38:22 +0100

    Completed Stage 2 of the project. - game now enables the user to win

--- Rock-Paper-Scissors/task/rps/game.py
+++ Rock-Paper-Scissors/task/rps/game.py
@@ -1,7 +1,26 @@
+import random
+
 choose = input()
+option = ["scissors", "paper", "rock"]
+computerChoice = random.choice(option)
 if (choose == "scissors"):
-    print("Sorry, but the computer chose rock")
+    if computerChoice == "scissors":
+        print(f'There is a draw ({computerChoice})')
+    elif computerChoice == "paper":
+        print(f'Well done. The computer chose {computerChoice} and failed')
+    else:
+        print(f'Sorry, but the computer chose {computerChoice}')
 elif (choose == "paper"):
-    print("Sorry, but the computer chose scissors")
+    if computerChoice == "scissors":
+        print(f'Sorry, but the computer chose {computerChoice}')
+    elif computerChoice == "paper":
+        print(f'There is a draw ({computerChoice})')
+    else:
+        print(f'Well done. The computer chose {computerChoice} and failed')
 else:
-    print("Sorry, but the computer chose paper")
+    if computerChoice == "scissors":
+        print(f'Well done. The computer chose {computerChoice} and failed')
+    elif computerChoice == "paper":
+        print(f'Sorry, but the computer chose {computerChoice}')
+    else:
+        print(f'There is a draw ({computerChoice})')
