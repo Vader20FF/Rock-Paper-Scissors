22f97dfba9534a43feb310f1111c9175309fbe52
229898 Janiszewski Łukasz <229898@edu.p.lodz.pl>
2021-02-12 11:58:52 +0100

    Completed Stage 4 of the project. - game was functional decomposed - introducing an user score functionality (reading from external file)

--- Rock-Paper-Scissors/task/rps/game.py
+++ Rock-Paper-Scissors/task/rps/game.py
@@ -1,37 +1,85 @@
 import random
 
-userInput = input()
-options = ["scissors", "paper", "rock"]
-while userInput != "!exit":
-    while userInput not in options:
-        if userInput == "!exit":
-            break
+userName = ...
+score = 0
+
+
+def choosingUser():
+    global userName
+
+    userName = input("Enter your name: ")
+    print("Hello,", userName)
+
+
+def checkRating():
+    global userName
+    global score
+
+    file = open("rating.txt", mode="r")
+    for line in file:
+        if userName in line:
+            stripped = line.split()
+            score = int(stripped[1])
+    file.close()
+
+
+def choosingOption():
+    options = ['rock', 'paper', 'scissors', '!rating', '!exit']
+    optionChoose = input()
+    while optionChoose not in options:
         print("Invalid input")
-        userInput = input()
-    if userInput == "!exit":
-        break
+        optionChoose = input()
+    return optionChoose
+
+
+def gameLogic(userChoice):
+    global score
+    options = ['rock', 'paper', 'scissors']
     computerChoice = random.choice(options)
-    if userInput == "scissors":
+    if userChoice == "scissors":
         if computerChoice == "scissors":
             print(f'There is a draw ({computerChoice})')
+            score += 50
         elif computerChoice == "paper":
             print(f'Well done. The computer chose {computerChoice} and failed')
+            score += 100
         else:
             print(f'Sorry, but the computer chose {computerChoice}')
-    elif userInput == "paper":
+    elif userChoice == "paper":
         if computerChoice == "scissors":
             print(f'Sorry, but the computer chose {computerChoice}')
         elif computerChoice == "paper":
             print(f'There is a draw ({computerChoice})')
+            score += 50
         else:
             print(f'Well done. The computer chose {computerChoice} and failed')
+            score += 100
     else:
         if computerChoice == "scissors":
             print(f'Well done. The computer chose {computerChoice} and failed')
+            score += 100
         elif computerChoice == "paper":
             print(f'Sorry, but the computer chose {computerChoice}')
         else:
             print(f'There is a draw ({computerChoice})')
-    userInput = input()
+            score += 50
 
-print("Bye!")
+
+def playGame():
+    global userName
+    global score
+
+    userChoice = choosingOption()
+    while userChoice != "!exit":
+        if userChoice == "!rating":
+            print("Your rating:", score)
+            userChoice = choosingOption()
+        else:
+            gameLogic(userChoice)
+            userChoice = choosingOption()
+    print("Bye!")
+
+
+choosingUser()
+checkRating()
+playGame()
